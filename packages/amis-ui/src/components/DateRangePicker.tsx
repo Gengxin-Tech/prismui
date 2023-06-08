baidu/amis

feat(amis-editor;amis):时间范围组件快捷键自定义支持固定值的配置 + 设计器快捷键配置功能

packages/amis-ui/src/components/DateRangePicker.tsx
@@ -1096,11 +1096,13 @@ export class DateRangePicker extends React.Component<
                       FormulaExec['formula'](rangeRaw.startDate, data),
                       format
                     )
+                  : typeof rangeRaw.startDate === 'string'
+                  ? moment(rangeRaw.startDate, format)
                   : rangeRaw.startDate;
 
                 return startDate &&
                   moment.isMoment(startDate) &&
-                  startDate?.isValid()
+                  startDate.isValid()
                   ? startDate
                   : (item as ShortCutDateRange).startDate;
               },
@@ -1110,9 +1112,11 @@ export class DateRangePicker extends React.Component<
                       FormulaExec['formula'](rangeRaw.endDate, data),
                       format
                     )
-                  : rangeRaw.startDate;
+                  : typeof rangeRaw.endDate === 'string'
+                  ? moment(rangeRaw.endDate, format)
+                  : rangeRaw.endDate;
 
-                return endDate && moment.isMoment(endDate) && endDate?.isValid()
+                return endDate && moment.isMoment(endDate) && endDate.isValid()
                   ? endDate
                   : (item as ShortCutDateRange).endDate;
               }

Gengxin-Tech/prismui

packages/amis-ui/src/components/DateRangePicker.tsx
@@ -1112,11 +1112,13 @@ export class DateRangePicker extends React.Component<
                       FormulaExec['formula'](shortcutRaw.startDate, data),
                       format
                     )
+                  : typeof shortcutRaw.startDate === 'string'
+                  ? moment(shortcutRaw.startDate, format)
                   : shortcutRaw.startDate;
 
                 return startDate &&
                   moment.isMoment(startDate) &&
-                  startDate?.isValid()
+                  startDate.isValid()
                   ? startDate
                   : (item as ShortCutDateRange).startDate;
               },
@@ -1126,9 +1128,11 @@ export class DateRangePicker extends React.Component<
                       FormulaExec['formula'](shortcutRaw.endDate, data),
                       format
                     )
-                  : shortcutRaw.startDate;
+                  : typeof shortcutRaw.endDate === 'string'
+                  ? moment(shortcutRaw.endDate, format)
+                  : shortcutRaw.endDate;
 
-                return endDate && moment.isMoment(endDate) && endDate?.isValid()
+                return endDate && moment.isMoment(endDate) && endDate.isValid()
                   ? endDate
                   : (item as ShortCutDateRange).endDate;
               }